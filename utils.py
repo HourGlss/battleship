15972500dd784c54c35137f0ebb0d44df20b9cca
Hourglss <misterfyxit@gmail.com>
2024-02-19 18:09:23 -0500

    I think I'm done?

---
+++ utils.py
@@ -0,0 +1,23 @@
+from enum import Enum, auto
+
+
+class ShipRotation(Enum):
+    UP = 3
+    DOWN = 2
+    LEFT = 1
+    RIGHT = 0
+
+
+class BattleShipException(Exception):
+    pass
+
+
+class TileState(Enum):
+    EMPTY = " "
+    EMPTY_MISS = "."
+    BOAT_HIT = "x"
+    CARRIER = "C"
+    BATTLESHIP = "B"
+    SUBMARINE1 = "S"
+    SUBMARINE2 = "s"
+    DESTROYER = "D"
