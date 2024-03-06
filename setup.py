b12a85d45ead7d0a57f66f06eab8eb2d087f45fa
fleshmanj <fleshmanjoshuac@hotmail.com>
2024-03-06 00:15:44 -0600

    more refactoring

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='battleship',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(include=['pocs', 'Battleship', 'battleship']),
     # include or exclude patterns as necessary
 )
