a067ea1d90e3ed2b017e258717d95af1606fd9bf
fleshmanj <fleshmanjoshuac@hotmail.com>
2024-03-09 01:18:12 -0600

    more crypto

--- src/events.py
+++ src/events.py
@@ -40,14 +40,13 @@ def find_open_windows_port():
 
 
 @socketio.on("connect")
-def handle_connect(data):
-    print(data)
+def handle_connect(args):
     print("Client Connected")
     print(request)
     print(request.data)
     clientid = get_random_bytes(16)
     username = request.headers.get("username")
-    rec_key = base64.urlsafe_b64decode(request.headers.get("rec_key").encode("utf-8"))
+    rec_key = base64.urlsafe_b64decode(args[1].get("rec_key").encode("utf-8"))
     print(rec_key)
     message = secure_server.initial_send(username, rec_key)
     socketio.emit("initial send", {"auth": message}, to=request.sid)
