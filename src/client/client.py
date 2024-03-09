521460c80663b3d63777c9fdf68160a07d4e9edc
fleshmanj <fleshmanjoshuac@hotmail.com>
2024-03-09 00:37:44 -0600

    more crypto

--- src/client/client.py
+++ src/client/client.py
@@ -62,7 +62,7 @@ class Client:
     def start(self):
         # Connect using the client instance to the specified URI
         print(self.secure_player.pub_rsa)
-        self.sio.connect(self.uri, headers={"username": self.name}, auth={"rec_key": self.secure_player.pub_rsa.decode("utf-8")})
+        self.sio.connect(self.uri, headers={"username": self.name}, auth={"rec_key": self.secure_player.pub_rsa})
         print("Client started and connected to", self.uri)
         client.sio.emit("register", {"username": self.name, "payload": self.secure_player.send_data("Registering")})
 
--- src/events.py
+++ src/events.py
@@ -43,7 +43,7 @@ def handle_connect(auth):
     print("Client Connected")
     clientid = get_random_bytes(16)
     username = request.headers.get("username")
-    rec_key = auth.get("rec_key").encode("utf-8")
+    rec_key = auth.get("rec_key")
     print(rec_key)
     message = secure_server.initial_send(username, rec_key)
     socketio.emit("initial send", {"auth": message}, to=request.sid)
