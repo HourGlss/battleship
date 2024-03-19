0c6e1f13c1780eee7e2bda6c8cc7aa44e34b0dce
fleshmanj <fleshmanjoshuac@hotmail.com>
2024-03-19 16:15:33 -0500

    more testing

--- src/game_controller.py
+++ src/game_controller.py
@@ -76,7 +76,7 @@ class GameController(Thread):
                 self.battleship.make_move(player_index, data["x"], data["y"])
                 p1, p2 = self.battleship.check_game_over()
                 if p1 == False or p2 == False:
-                    self.socketio.emit("response", {"message": "Game over you won"}, room=request.sid)
+                    self.socketio.emit("response", {"message": "Game over you won", "board": self.battleship.print_board(player_index)}, room=request.sid)
             else:
                 self.socketio.emit("response", {"message": "Not your turn to move"}, room=request.sid)
 
